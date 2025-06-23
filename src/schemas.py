5a779e1f2f997f1040628229be968f52ad39536c
Andrey-Sherbakov <63076931+Andrey-Sherbakov@users.noreply.github.com>
2025-06-23 22:03:42 +0300

    Create Dockerfile, docker-compose.yml

--- src/schemas.py
+++ src/schemas.py
@@ -2,13 +2,7 @@ from pydantic import BaseModel, EmailStr, Field
 
 
 class EmailBody(BaseModel):
-    correlation_id: str
     subject: str
     body: str
     recipients: list[EmailStr]
     subtype: str = "plain"
-
-
-class EmailCallbackBody(BaseModel):
-    correlation_id: str
-    message: str
