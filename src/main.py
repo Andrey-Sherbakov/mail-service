166c8da822795c4b5eee2931df9bfde6246c89cc
Andrey-Sherbakov <63076931+Andrey-Sherbakov@users.noreply.github.com>
2025-07-07 23:15:54 +0300

    refactoring: telegram_bot

--- src/main.py
+++ src/main.py
@@ -18,7 +18,7 @@ async def lifespan(app: FastAPI):
     await bot_startup(app=app)
     await consumer_startup(app=app)
 
-    # await app.state.bot.send_message("Mail-service app started")
+    await app.state.bot.send_message("Mail-service app started")
 
     yield
 
